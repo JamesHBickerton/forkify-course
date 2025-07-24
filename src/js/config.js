2b16fce395c30a021df4a32410803e73bfda7d0c
JamesHBickerton <jameshuiabickerton@gmail.com>
2025-07-24 16:37:57 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '15cea0f0-f997-4649-816f-9e94dbe4873e';
+export const MODAL_CLOSE_SEC = 2.5;
